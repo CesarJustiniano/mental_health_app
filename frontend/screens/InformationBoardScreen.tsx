9c75f3e83aa0565731bbeec6808c1963d3be973c
CesarJustiniano <cesar.justiniano@upr.edu>
2021-05-06 18:21:36 -0400

    added linking import in info board screen

--- frontend/screens/InformationBoardScreen.tsx
+++ frontend/screens/InformationBoardScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {KeyboardAvoidingView, ScrollView, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
+import {KeyboardAvoidingView, Linking, ScrollView, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 
 import {Text, View, } from '../components/Themed';
